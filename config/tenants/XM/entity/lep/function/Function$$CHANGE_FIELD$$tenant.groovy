61b39505f9e9f3ae7ba3a074e5759c5b144d3056
alexmold <a.moldavsky@atompark.com>
2021-07-23 16:44:04 +0300

    specs modif

--- config/tenants/XM/entity/lep/function/Function$$CHANGE_FIELD$$tenant.groovy
+++ config/tenants/XM/entity/lep/function/Function$$CHANGE_FIELD$$tenant.groovy
@@ -1,1 +1,31 @@
-return null
+import com.icthh.xm.ms.entity.domain.XmEntity
+import com.icthh.xm.ms.entity.domain.ext.IdOrKey
+import com.icthh.xm.ms.entity.service.XmEntityService
+import com.icthh.xm.ms.entity.domain.XmEntity
+
+//import org.slf4j.LoggerFactory
+
+//def xmEntity = lepContext.inArgs.xmEntity
+
+//def log = LoggerFactory.getLogger(getClass())
+
+XmEntityService entityService = lepContext.services.xmEntity
+
+IdOrKey idOrKey = lepContext.inArgs?.idOrKey
+
+newPrice = lepContext.inArgs?.functionInput?.newPrice
+
+XmEntity myEntity = entityService.findOne(idOrKey)
+
+if (myEntity.typeKey.startsWith("TARIFF")) {
+
+
+
+
+    tariff.data.tariffPrice = newPrice
+
+    entityService.save(tariff)
+
+}
+
+return [:]
