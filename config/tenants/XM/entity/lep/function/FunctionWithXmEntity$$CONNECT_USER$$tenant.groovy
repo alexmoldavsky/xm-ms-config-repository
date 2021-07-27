ce9dc50d448698f7840a3d94afe61c36eff1fe9a
alexmold <a.moldavsky@atompark.com>
2021-07-27 14:32:04 +0300

    specs modif

---
+++ config/tenants/XM/entity/lep/function/FunctionWithXmEntity$$CONNECT_USER$$tenant.groovy
@@ -0,0 +1,32 @@
+import com.icthh.xm.ms.entity.service.LinkService
+import com.icthh.xm.ms.entity.domain.Link
+import com.icthh.xm.ms.entity.domain.XmEntity
+import com.icthh.xm.ms.entity.domain.ext.IdOrKey
+import com.icthh.xm.ms.entity.service.XmEntityService
+
+
+//def tariffConnection = new XmEntity()
+//tariffConnection.setKey(UUID.randomUUID().toString())
+//tariffConnection.setName("${orderData.salesChannel} ${orderData.sapOrderId}")
+//tariffConnection.setData(orderData)
+//tariffConnection.typeKey('ORDER')
+
+//entityService.save(order)
+
+IdOrKey idOrKey = lepContext.inArgs.idOrKey
+XmEntityService xmEntityService = lepContext.services.xmEntity
+XmEntity xmEntity = xmEntityService.findOne(idOrKey)
+
+LinkService linkService = lepContext.services.linkService
+Link link = new Link()
+link.setName('TARIFF-TO-USER')
+link.setTypeKey('TARIFF-TO-USER')
+link.setStartDate(new Date().toInstant())
+link.setSource(source)
+link.setTarget(xmEntity)
+linkService.save(link)
+
+return [:]
+
+
+
